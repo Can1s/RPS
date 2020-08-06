c550e2881e81f0db9d3844ac668340b8d86ee926
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-08-06 14:04:14 +0600

    Stage 3/5
    Complete the third stage

--- game.py
+++ game.py
@@ -1,16 +1,23 @@
 import random
 
 rps = ["rock", "scissors", "paper"]
-user = input()
-rnd = random.choice(rps)
-if user.__eq__(rnd):
-    print(f"There is a draw ({user})")
-else:
-    if user.__eq__("rock") and rnd.__eq__("scissors"):
-        print(f"Well done. Computer chose {rnd} and failed")
-    elif user.__eq__("scissors") and rnd.__eq__("paper"):
-        print(f"Well done. Computer chose {rnd} and failed")
-    elif user.__eq__("paper") and rnd.__eq__("rock"):
-        print(f"Well done. Computer chose {rnd} and failed")
+while True:
+    user = input()
+    rnd = random.choice(rps)
+    if user.__eq__("!exit"):
+        print("Bye!")
+        break
+    elif user.__eq__("rock") or user.__eq__("scissors") or user.__eq__("paper"):
+        if user.__eq__(rnd):
+            print(f"There is a draw ({user})")
+        else:
+            if user.__eq__("rock") and rnd.__eq__("scissors"):
+                print(f"Well done. Computer chose {rnd} and failed")
+            elif user.__eq__("scissors") and rnd.__eq__("paper"):
+                print(f"Well done. Computer chose {rnd} and failed")
+            elif user.__eq__("paper") and rnd.__eq__("rock"):
+                print(f"Well done. Computer chose {rnd} and failed")
+            else:
+                print(f"Sorry, but computer chose {rnd}")
     else:
-        print(f"Sorry, but computer chose {rnd}")
+        print("Invalid input")
